b8c448508ba0b826d8e8c677862bd5be36f1a1d9
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2018-08-02 20:39:25 +0200

    adopted from uptream: nl80211: allow multiple active
    nl80211: allow multiple active scheduled scan requests
    commit id
    ca986ad9bcd3893c8b0b4cc2cafcc8cf1554409c
    
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -6828,7 +6828,11 @@ static void rtw_cfg80211_preinit_wiphy(_adapter *adapter, struct wiphy *wiphy)
 #endif
 
 #if defined(CONFIG_PM) && (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 0, 0))
-	wiphy->flags |= WIPHY_FLAG_SUPPORTS_SCHED_SCAN;
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0))
+	wiphy->max_sched_scan_reqs = 1;
+#else
+ 	wiphy->flags |= WIPHY_FLAG_SUPPORTS_SCHED_SCAN;
+#endif
 #ifdef CONFIG_PNO_SUPPORT
 	wiphy->max_sched_scan_ssids = MAX_PNO_LIST_COUNT;
 #endif
