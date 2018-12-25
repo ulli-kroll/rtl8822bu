698539243001706e338b8f68dd01d555833f2b50
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2018-12-25 15:30:57 +0100

    v4.20 get_monotonic_boottime() fix
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -349,8 +349,12 @@ static const struct ieee80211_txrx_stypes
 static u64 rtw_get_systime_us(void)
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,20,0))
+	struct timespec64 ts = current_kernel_time64();
+#else
 	struct timespec ts;
 	get_monotonic_boottime(&ts);
+#endif
 	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
 #else
 	struct timeval tv;
