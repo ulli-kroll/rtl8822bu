74c52b72db569999548a51ae6af2c788c9dac3f5
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2020-08-06 16:28:13 +0200

    fix v5.8 remove unused sha256.
    This causes build errors
    
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- include/rtw_security.h
+++ include/rtw_security.h
@@ -239,12 +239,6 @@ struct security_priv {
 #endif /* DBG_SW_SEC_CNT */
 };
 
-struct sha256_state {
-	u64 length;
-	u32 state[8], curlen;
-	u8 buf[64];
-};
-
 #define GET_ENCRY_ALGO(psecuritypriv, psta, encry_algo, bmcst)\
 	do {\
 		switch (psecuritypriv->dot11AuthAlgrthm) {\
